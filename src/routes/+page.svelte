aa5f4b10b39bce547df97901e4a375c7b149dadd
bvvst <lukeimbing@gmail.com>
2024-11-14 18:21:32 -0500

    more to-do's

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
   import { Store } from "@tauri-apps/plugin-store";
   import { goto } from "$app/navigation";
 
+  // todo: dummy page with dummy data. mockups here:
+  // https://www.figma.com/design/5BKeaIZWerEF18WGVNBlsI/Scope?node-id=103-15&t=dEKdon9m1HOUlC5b-1
+
   // todo:
   // secure token access and storage will be implemented with:
   // https://github.com/tauri-apps/plugins-workspace/tree/v1/plugins/stronghold
@@ -24,6 +27,9 @@
     }
 
     console.log("token", result.value);
+
+    // todo: check local storage for last open path, then
+    // goto(/channel/[channel_id])
   });
 </script>
 
